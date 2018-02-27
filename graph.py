89d98ce97bc6d92ae53c4bdbe8278e98c8df82fe
Tahir Nurmiev <tnur92@gmail.com>
2018-02-27 15:25:30 +0300

    13961 v2

--- graph.py
+++ graph.py
@@ -24,24 +24,31 @@ class Graph():
 def get_part_of_speech(tag):
     if tag in parts_of_speech:
         return parts_of_speech[tag]
-    return 'Punctuation mark'
+    return 'Unknown part of speech:' + tag
 
 def get_dependent(rel):
     if rel in dependencies:
         return dependencies[rel]
-    return 'Unknown dependence'
+    return 'Unknown dependence: ' + rel
 
+# relcl
 dependencies = {
+'acl': 'clausal modifier of noun (adjectival clause)',
 'acomp': 'adjectival complement',
 'advcl': 'adverbial clause modifier',
 'advmod': 'adverb modifier',
 'agent': 'agent',
 'amod': 'adjectival modifier',
 'appos': 'appositional modifier',
+'attr': 'attribute',
 'aux': 'auxiliary',
 'auxpass': 'passive auxiliary',
+'case': 'case marking',
 'cc': 'coordination',
 'ccomp': 'clausal complement',
+'clf': 'classifier',
+'compound': 'compound',
+'complm': 'complementizer',
 'conj': 'conjunct',
 'cop': 'copula',
 'csubj': 'clausal subject',
@@ -49,20 +56,36 @@ dependencies = {
 'dep': 'dependent',
 'det': 'determiner',
 'discourse': 'discourse element',
+'dislocated': 'dislocated elements',
 'dobj': 'direct object',
 'expl': 'expletive',
+'fixed': 'fixed multiword expression',
+'flat': 'flat multiword expression',
+'hmod': 'modifier in hyphenation',
+'hyph': 'hyphen',
 'goeswith': 'goes with',
+'infmod': 'infinitival modifier',
+'intj': 'interjection',
 'iobj': 'indirect object',
+'list': 'list',
 'mark': 'marker',
+'meta': 'meta modifier',
 'mwe': 'multi-word expression',
 'neg': 'negation modifier',
+'nmod': 'modifier of nominal',
 'nn': 'noun compound modifier',
 'npadvmod': 'noun phrase as adverbial modifier',
 'nsubj': 'nominal subject',
 'nsubjpass': 'passive nominal subject',
 'num': 'numeric modifier',
+'nummod': 'numeric modifier',
 'number': 'element of compound number',
+'oprd': 'object predicate',
+'obj': 'object',
+'obl': 'oblique nominal',
+'orphan': 'orphan',
 'parataxis': 'parataxis',
+'partmod': 'participal modifier',
 'pcomp': 'prepositional complement',
 'pobj': 'object of a preposition',
 'poss': 'possession modifier',
@@ -76,25 +99,46 @@ dependencies = {
 'quantmod': 'quantifier phrase modifier',
 'rcmod': 'relative clause modifier',
 'ref': 'referent',
+'relcl': 'relative clause modifier',
+'reparandum': 'overridden disfluency',
 'root': 'root',
 'tmod': 'temporal modifier',
 'vmod': 'reduced non-finite verbal modifier',
+'vocative': 'vocative',
 'xcomp': 'open clausal complement',
 'xsubj': 'controlling subject'
 }
 
 parts_of_speech = {
+'-LRB-': 'left round bracket',
+'-RRB-': 'right round bracket',
+',': 'punctuation mark, comma',
+':': 'punctuation mark, colon or ellipsis',
+'.': 'punctuation mark, sentence closer',
+"''": 'closing quotation mark',
+'""': 'closing quotation mark',
+'#': 'symbol, number sign',
+'``': 'opening quotation mark',
+'$': 'symbol, currency',
+'ADD': 'email',
+'AFX': 'affix',
+'BES': 'auxiliary "be"',
 'CC': 'Coordinating conjunction',
 'CD': 'Cardinal number',
 'DT': 'Determiner',
 'EX': 'Existential there',
 'FW': 'Foreign word',
+'GW': 'additional word in multi-word expression',
+'HVS': 'forms of "have"',
+'HYPH': 'PunctType=dash  punctuation mark, hyphen',
 'IN': 'Preposition or subordinating conjunction',
 'JJ': 'Adjective',
 'JJR': 'Adjective, comparative',
 'JJS': 'Adjective, superlative',
 'LS': 'List item marker',
 'MD': 'Modal',
+'NFP': 'Superfluous punctuation',
+'NIL': 'Missing tag',
 'NN': 'Noun, singular or mass',
 'NNS': 'Noun, plural',
 'NNP': 'Proper noun, singular',
@@ -107,6 +151,7 @@ parts_of_speech = {
 'RBR': 'Adverb, comparative',
 'RBS': 'Adverb, superlative',
 'RP': 'Particle',
+'_SP': 'Space',
 'SYM': 'Symbol',
 'TO': 'to',
 'UH': 'Interjection',
@@ -119,5 +164,6 @@ parts_of_speech = {
 'WDT': 'Wh-determiner',
 'WP': 'Wh-pronoun',
 'WP$': 'Possessive wh-pronoun',
-'WRB': 'Wh-adverb'
+'WRB': 'Wh-adverb',
+'XX': 'Unknown'
 }
