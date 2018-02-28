f18e048eeab31235090c08c3eb4b68b4ac055041
Tahir Nurmiev <tnur92@gmail.com>
2018-02-28 18:38:37 +0300

    reset

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ print('Run server')
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         # self.render('index.html', debug=debug)
-        self.render('index.html', debug='false')
+        self.render('index.html', debug=debug)
 
 class ProcessSentenceHandler(tornado.web.RequestHandler):
     def post(self):
@@ -35,22 +35,22 @@ application = tornado.web.Application([
 )
 
 if __name__ == '__main__':
-    wsgi_app = tornado.wsgi.WSGIAdapter(application)
-    server = wsgiref.simple_server.make_server('', 8888, wsgi_app)
-    server.serve_forever()
-    # try:
-    #     f = open('config.json','r')
-    #     config = json.load(f)
-    #     protocol = config['server-protocol']
-    #     address = config['server-ip']
-    #     port = config['server-port']
-    #     debug = config['debug']
-    #     host_url = protocol + address + ':' + port + '/'
-    #     print(host_url)
-    # except Exception as e:
-    #     print('Can not load file config.json:', e)
-    # else:
-    #     print ('loading server')
-    #     application.listen(int(port))
-    #     print ('starting')
-    #     tornado.ioloop.IOLoop.instance().start()
+    # wsgi_app = tornado.wsgi.WSGIAdapter(application)
+    # server = wsgiref.simple_server.make_server('', 8888, wsgi_app)
+    # server.serve_forever()
+    try:
+        f = open('config.json','r')
+        config = json.load(f)
+        protocol = config['server-protocol']
+        address = config['server-ip']
+        port = config['server-port']
+        debug = config['debug']
+        host_url = protocol + address + ':' + port + '/'
+        print(host_url)
+    except Exception as e:
+        print('Can not load file config.json:', e)
+    else:
+        print ('loading server')
+        application.listen(int(port))
+        print ('starting')
+        tornado.ioloop.IOLoop.instance().start()
